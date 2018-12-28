a92c48a8fb7ccd65675b3a50a218f99a531b79b2
Raphael <raphael.hauk@racing.tuwien.ac.at>
2018-12-29 00:10:22 +0100

    refactor client interface

--- CalendarService.Client/CalendarServiceClient.cs
+++ CalendarService.Client/CalendarServiceClient.cs
@@ -2,9 +2,11 @@
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using OAuthApiClient.Abstractions;
+using System;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace CalendarService.Client
 {
@@ -12,6 +14,161 @@ namespace CalendarService.Client
     {
         private readonly IAuthenticationProvider authenticationProvider;
         private readonly CalendarServiceOptions options;
+
+        public IUserCollection Users => new UserCollection(GetClientAsync);
+
+        private class UserCollection : IUserCollection
+        {
+            private readonly Func<Task<HttpClient>> clientFactory;
+
+            public UserCollection(Func<Task<HttpClient>> clientFactory)
+            {
+                this.clientFactory = clientFactory;
+            }
+
+            public IUser this[string userId] => new UserClient(userId, clientFactory);
+        }
+
+        private class ReminderCollection : IReminderCollection
+        {
+            private readonly string userId;
+            private readonly Func<Task<HttpClient>> clientFactory;
+
+            public ReminderCollection(string userId, Func<Task<HttpClient>> clientFactory)
+            {
+                this.userId = userId;
+                this.clientFactory = clientFactory;
+            }
+
+            public IReminder this[string reminderId] => new ReminderClient(userId, reminderId, clientFactory);
+
+            public async Task<ReminderRegistration> RegisterAsync(ReminderRequest request)
+            {
+                var res = await (await clientFactory()).PostAsync($"api/users/{userId}/reminders",
+                    new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json"));
+                if (res.IsSuccessStatusCode)
+                {
+                    return JsonConvert.DeserializeObject<ReminderRegistration>(await res.Content.ReadAsStringAsync());
+                }
+                throw new CalendarServiceException($"Could not register reminder: {res.StatusCode}.");
+            }
+        }
+
+        private class EventCollection : IEventCollection
+        {
+            private readonly string userId;
+            private readonly Func<Task<HttpClient>> clientFactory;
+
+            public EventCollection(string userId, Func<Task<HttpClient>> clientFactory)
+            {
+                this.userId = userId;
+                this.clientFactory = clientFactory;
+            }
+
+            public async Task<Event[]> Get(DateTime? from, DateTime? to)
+            {
+                StringBuilder uriBuilder = new StringBuilder($"api/calendar/{userId}");
+                JsonSerializerSettings microsoftDateFormatSettings = new JsonSerializerSettings
+                {
+                    DateFormatHandling = DateFormatHandling.IsoDateFormat
+                };
+                if (from.HasValue)
+                {
+                    uriBuilder.Append($"?from={HttpUtility.UrlEncode(JsonConvert.SerializeObject(from.Value))}");
+                    if (to.HasValue)
+                    {
+                        uriBuilder.Append($"&to={HttpUtility.UrlEncode(JsonConvert.SerializeObject(to.Value))}");
+                    }
+                }
+                else if (to.HasValue)
+                {
+                    uriBuilder.Append($"?to={HttpUtility.UrlEncode(JsonConvert.SerializeObject(to.Value))}");
+                }
+
+                var res = await (await clientFactory()).GetAsync(uriBuilder.ToString());
+                if (res.IsSuccessStatusCode)
+                {
+                    var content = await res.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<Event[]>(content);
+                }
+                else if (res.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    return null;
+                }
+                throw new CalendarServiceException($"Could not retrieve events: {res.StatusCode}");
+            }
+
+            public async Task<Event> GetCurrentAsync()
+            {
+                var res = await (await clientFactory()).GetAsync($"api/calendar/{userId}/current");
+                if (res.IsSuccessStatusCode)
+                {
+                    var content = await res.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<Event>(content);
+                }
+                else if (res.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    return null;
+                }
+                throw new CalendarServiceException($"Could not retrieve current event: {res.StatusCode}");
+            }
+        }
+
+        private class UserClient : IUser
+        {
+            private readonly string userId;
+            private readonly Func<Task<HttpClient>> clientFactory;
+
+            public UserClient(string userId, Func<Task<HttpClient>> clientFactory)
+            {
+                this.userId = userId;
+                this.clientFactory = clientFactory;
+            }
+
+            public IReminderCollection Reminders => new ReminderCollection(userId, clientFactory);
+
+            public IEventCollection Events => new EventCollection(userId, clientFactory);
+        }
+
+        private class ReminderClient : IReminder
+        {
+            private readonly string userId;
+            private readonly string reminderId;
+            private readonly Func<Task<HttpClient>> clientFactory;
+
+            public ReminderClient(string userId, string reminderId, Func<Task<HttpClient>> clientFactory)
+            {
+                this.userId = userId;
+                this.reminderId = reminderId;
+                this.clientFactory = clientFactory;
+            }
+
+            public async Task<bool> IsAliveAsync()
+            {
+                var res = await (await clientFactory()).GetAsync($"api/users/{userId}/reminders/{reminderId}");
+                if (res.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+                else if (res.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    return false;
+                }
+                throw new CalendarServiceException($"Could retrieve reminder status: {res.StatusCode}.");
+            }
+
+            public async Task<ReminderRegistration> RenewAsync()
+            {
+                var request = new HttpRequestMessage(new HttpMethod("PATCH"),
+                $"api/users/{userId}/reminders/{reminderId}");
+                var res = await (await clientFactory()).SendAsync(request);
+                if (!res.IsSuccessStatusCode)
+                {
+                    throw new CalendarServiceException($"Could not renew reminder: {res.StatusCode}.");
+                }
+                return JsonConvert.DeserializeObject<ReminderRegistration>(await res.Content.ReadAsStringAsync());
+            }
+        }
 
         public CalendarServiceClient(IAuthenticationProvider authenticationProvider, IOptions<CalendarServiceOptions> optionsAccessor)
         {
@@ -27,59 +184,6 @@ namespace CalendarService.Client
             };
             await authenticationProvider.AuthenticateClient(client);
             return client;
-        }
-
-        public async Task<Event> GetCurrentEventAsync(string userId)
-        {
-            var res = await (await GetClientAsync()).GetAsync($"api/calendar/{userId}/current");
-            if (res.IsSuccessStatusCode)
-            {
-                var content = await res.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<Event>(content);
-            }
-            else if (res.StatusCode == System.Net.HttpStatusCode.NotFound)
-            {
-                return null;
-            }
-            throw new CalendarServiceException($"Could not retrieve current event: {res.StatusCode}");
-        }
-
-        public async Task<ReminderRegistration> RegisterReminderAsync(string userId, ReminderRequest registration)
-        {
-            var res = await (await GetClientAsync()).PostAsync($"api/users/{userId}/reminders",
-                new StringContent(
-                    JsonConvert.SerializeObject(registration), Encoding.UTF8, "application/json"));
-            if (res.IsSuccessStatusCode)
-            {
-                return JsonConvert.DeserializeObject<ReminderRegistration>(await res.Content.ReadAsStringAsync());
-            }
-            throw new CalendarServiceException($"Could not register reminder: {res.StatusCode}.");
-        }
-
-        public async Task<bool> ReminderAliveAsync(string userId, string reminderId)
-        {
-            var res = await (await GetClientAsync()).GetAsync($"api/users/{userId}/reminders/{reminderId}");
-            if (res.IsSuccessStatusCode)
-            {
-                return true;
-            }
-            else if (res.StatusCode == System.Net.HttpStatusCode.NotFound)
-            {
-                return false;
-            }
-            throw new CalendarServiceException($"Could retrieve reminder status: {res.StatusCode}.");
-        }
-
-        public async Task<ReminderRegistration> RenewReminderAsync(string userId, string id)
-        {
-            var request = new HttpRequestMessage(new HttpMethod("PATCH"),
-                $"api/users/{userId}/reminders/{id}");
-            var res = await (await GetClientAsync()).SendAsync(request);
-            if (!res.IsSuccessStatusCode)
-            {
-                throw new CalendarServiceException($"Could not renew reminder: {res.StatusCode}.");
-            }
-            return JsonConvert.DeserializeObject<ReminderRegistration>(await res.Content.ReadAsStringAsync());
         }
     }
 }
--- CalendarService.Client/ICalendarServiceClient.cs
+++ CalendarService.Client/ICalendarServiceClient.cs
@@ -1,13 +1,44 @@
 ﻿using CalendarService.Models;
+using System;
 using System.Threading.Tasks;
 
 namespace CalendarService.Client
 {
     public interface ICalendarServiceClient
     {
-        Task<Event> GetCurrentEventAsync(string userId);
-        Task<ReminderRegistration> RegisterReminderAsync(string userId, ReminderRequest request);
-        Task<ReminderRegistration> RenewReminderAsync(string userId, string id);
-        Task<bool> ReminderAliveAsync(string userId, string reminderId);
+        IUserCollection Users { get; }
+    }
+    public interface IUserCollection
+    {
+        IUser this[string userId]
+        {
+            get;
+        }
+    }
+    public interface IUser
+    {
+        IReminderCollection Reminders { get; }
+        IEventCollection Events { get; }
+    }
+
+    public interface IEventCollection
+    {
+        Task<Event> GetCurrentAsync();
+        Task<Event[]> Get(DateTime? from, DateTime? to);
+    }
+
+    public interface IReminderCollection
+    {
+        IReminder this[string reminderId]
+        {
+            get;
+        }
+        Task<ReminderRegistration> RegisterAsync(ReminderRequest request);
+    }
+
+    public interface IReminder
+    {
+        Task<ReminderRegistration> RenewAsync();
+        Task<bool> IsAliveAsync();
     }
 }
